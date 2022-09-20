39f4413f05998b3eaf6b6a09bd4172a4a2e50ad2
suyunlei <805118737@qq.com>
2022-09-20 20:43:16 +0800

    [update]

---
+++ 二叉树/104. 二叉树的最大深度.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (root == null) {
+    return 0;
+  }
+  let res = [];
+  let queue = [root];
+  while (queue.length) {
+    let curLevel = [];
+    let n = queue.length;
+    for (let i = 0; i < n; i++) {
+      let node = queue.shift();
+      curLevel.push(node.val);
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
+    }
+    res.push(curLevel);
+  }
+  return res.length;
+};
