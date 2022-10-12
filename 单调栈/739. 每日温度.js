b45dea7c9ea898caf25d5d6021b6e5b51c593345
suyunlei <805118737@qq.com>
2022-10-12 15:05:57 +0800

    [update]

---
+++ 单调栈/739. 每日温度.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function (temperatures) {
+  let n = temperatures.length;
+  let result = new Array(n).fill(0);
+  let stack = [0];
+  for (let i = 1; i < n; i++) {
+    debugger;
+    if (temperatures[i] <= temperatures[stack[stack.length - 1]]) {
+      stack.push(i);
+    } else {
+      while (temperatures[i] > temperatures[stack[stack.length - 1]]) {
+        let pop = stack.pop();
+        result[pop] = i - pop;
+      }
+      stack.push(i);
+    }
+  }
+  return result;
+};
+dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]);
